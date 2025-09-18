d7baac962fcde45e8e783b9071e2d1ba16e4e669
Andrea RH <andrea19deabril@gmail.com>
2025-09-18 11:04:42 +0200

    commit2

--- programa.py
+++ programa.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
-num1=float(input("Escribe un número: "))
-num2=float(input("Escribe otro número: "))
+num1=int(input("Escribe un número: "))
+num2=int(input("Escribe otro número: "))
 print("La suma de los dos número es: %d " %(num1+num2))
 print("La resta de los dos número es: %d " %(num1-num2))
+print("La multiplicación de los dos número es: %d " %(num1*num2))
+print("La multiplicación de los dos número es: %.2f " %(num1/num2))
+print("Suma: %d , resta: %d " %(num1+num2,num1-num2))
+
+print(f"La suma es {num1+num2}")
+print(f"La resta es {num1-num2}, y la multiplicacion es {num1*num2}")
+
+l=[1,'hola',5.9]
+meses=('enero','feb', 'marzo')
+p1={'nombre':'JI', 'edad':44}
+p2={'nombre':'Mercedes', 'edad':39}
+profes=[p1,p2]
