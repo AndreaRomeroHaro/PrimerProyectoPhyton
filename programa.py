568e0cb2a842da221b89860ba948a494534fdacd
Andrea RH <andrea19deabril@gmail.com>
2025-09-18 10:04:48 +0200

    primercommit

--- programa.py
+++ programa.py
@@ -1,2 +1,5 @@
-
-print('hola')
+#!/usr/bin/env python
+num1=float(input("Escribe un número: "))
+num2=float(input("Escribe otro número: "))
+print("La suma de los dos número es: %d " %(num1+num2))
+print("La resta de los dos número es: %d " %(num1-num2))
